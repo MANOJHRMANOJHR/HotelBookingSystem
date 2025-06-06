05723154998e2584d534afcb48c89d5aa8511a3f
MANOJHRMANOJHR <manojhr799@gmail.com>
2025-06-06 14:44:08 +0530

    mysqlmetadataupdate

--- backend/index.js
+++ backend/index.js
@@ -13,11 +13,11 @@ app.use(bodyParser.json());
 
 // Set up MySQL connection pool
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST1,
+    user: process.env.DB_USER1,
+    password: process.env.DB_PASSWORD1,
+    database: process.env.DB_NAME1,
+    port: process.env.DB_PORT1,
 });
 
 // Test database connection
